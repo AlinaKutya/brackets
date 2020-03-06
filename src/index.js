6c5341cb148014e6b9837b9930a26c33c45732df
Alina Kutyeva <flytta.dig@mail.ru>
2020-03-06 15:30:05 +0300

    feat: check 1st version of solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    console.log("первый параметр, строка:");
+    console.log(str);
+    console.log(" ");
+    console.log("второй параметр, массив:");
+    console.log(bracketsConfig);
+
+    function check2(str) {
+        let stack = [];
+
+        if (isFinite(str)) {
+            stack.push(str[0]);
+
+            for (let i = 1; i < str.length; i++) {
+                if (str[i] == stack[stack.length - 1]) {
+                    stack.pop();
+                } else {
+                    stack.push(str[i]);
+                }
+            }
+            console.log("оставшиеся символы без пары: ", stack);
+            if (!stack.length) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] == "(" || str[i] == "[" || str[i] == "{") {
+                stack.push(str[i]);
+            } else if (str[i] == "|") {
+                if (stack[stack.length - 1] == "|") {
+                    stack.pop();
+                } else {
+                    stack.push(str[i]);
+                }
+            } else {
+                if (!stack.length) {
+                    return false;
+                }
+                if (
+                    (stack[stack.length - 1] == "(" && str[i] !== ")") ||
+                    (stack[stack.length - 1] == "[" && str[i] !== "]") ||
+                    (stack[stack.length - 1] == "{" && str[i] !== "}")
+                ) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        if (stack.length) {
+            return false;
+        }
+
+        return true;
+    }
+
+    let stringCheck,
+        arrayCheck,
+        brackets = [].concat(...bracketsConfig).join("");
+
+    stringCheck = check2(str);
+    arrayCheck = check2(brackets);
+    console.log("stringCheck ", stringCheck);
+    console.log("arrayCheck ", arrayCheck);
+    // console.log(stringCheck == true);
+    // console.log(arrayCheck == true);
+
+    if (stringCheck && arrayCheck) {
+        console.log("результат true");
+        return true;
+    } else {
+        console.log("результат false");
+        return false;
+    }
+};
